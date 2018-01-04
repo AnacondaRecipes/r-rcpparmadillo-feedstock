4bfca3bfbd32dbc0996857303deeaafbf1c563f9
Ray Donnelly <mingw.android@gmail.com>
2018-01-04 22:03:34 +0000

    Removal of LDFLAGS_CC on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  export LDFLAGS="${LDFLAGS_CC} -Wl,-rpath,${PREFIX}/lib"
-fi
-
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
